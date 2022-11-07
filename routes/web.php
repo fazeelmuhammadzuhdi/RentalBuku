078415b5ce04c789f6414533dff493896e47f1f8
fazeelmuhammadzuhdi <fazeelmuhammadzuhdi@gmail.com>
2022-11-07 07:54:08 +0700

    Login, Logout and Middleware

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,9 @@
 <?php
 
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\BookController;
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -7,7 +11,7 @@ use Illuminate\Support\Facades\Route;
 | Web Routes
 |--------------------------------------------------------------------------
 |
-| Here is where you can register web routes for your application. These
+| Here is where you can profile web routes for your application. These
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
 |
@@ -15,4 +19,18 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+})->middleware('auth');
+
+Route::middleware('only_guest')->group(function () {
+    Route::get('login', [AuthController::class, 'login'])->name('login');
+    Route::post('login', [AuthController::class, 'authenticating']);
+    Route::get('register', [AuthController::class, 'register'])->name('register');
+});
+
+
+Route::middleware('auth')->group(function () {
+    Route::get('logout', [AuthController::class, 'logout']);
+    Route::get('dashboard', [DashboardController::class, 'index'])->middleware('only_admin');
+    Route::get('profile', [UserController::class, 'profile'])->middleware('only_client');
+    Route::get('books', [BookController::class, 'index']);
 });
